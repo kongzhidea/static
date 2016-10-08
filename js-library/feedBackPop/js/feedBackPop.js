96a446e60ac9219ffa977d287a7a7c7573b62335
kongzhidea <563150913@qq.com>
2016-10-08 15:07:28 +0800

    feedbackpop

---
+++ js-library/feedBackPop/js/feedBackPop.js
@@ -0,0 +1,18 @@
+$.feedBackPop = function(text) {
+    var self = this;
+    var marginTop = ($(window).height() - $(window).width() * 0.243442623) / 2;
+    $('.popmask').remove();
+
+    var $maskHtml = $("<div/>", {
+        class: "popmask"
+    }).html('<div class="popPromptText" style="margin-top:' + marginTop + 'px">' + text + '</div>')
+        .appendTo($('body'));
+
+    setTimeout(function() {
+        $('.popmask').remove();
+    }, 1500);
+    //禁止滚动
+    $('.popmask').bind('touchmove', function() {
+        return false;
+    });
+}
